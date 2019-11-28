d9669f937b95ff2de668cc92f28cf162886c5085
Jonatan <jonatanav255@hotmail.com>
2019-11-28 00:26:55 -0600

    Commit Final

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -22,6 +22,22 @@ function App() {
         setWildPokemon(response.data);
       });
   };
+  const catchPokemon = pokemon => {
+    setPokedex(state => {
+      const monExists = state.filter(p => pokemon.id == p.id).length > 0;
+      if (!monExists) {
+        state = [...state, pokemon];
+        state.sort(function(a, b) {
+          return a.id - b.id;
+        });
+      }
+      return state;
+    });
+    encounterWildPokemon();
+  };
+  const releasePokemon = id => {
+    setPokedex(state => state.filter(p => p.id != id));
+  };
 
   return (
     <div className="app-wrapper">
@@ -40,10 +56,33 @@ function App() {
           className="sprite"
         />
         <h3>{wildPokemon.name}</h3>
-        <button className="catch-btn">Catch</button>
+        <button className="catch-btn" onClick={() => catchPokemon(wildPokemon)}>
+          Catch
+        </button>
       </section>
       <section className="pokedex">
-        <h2></h2>
+        <h2>Pokedex</h2>
+        <div className="pokedex-list">
+          {pokedex.map(pokemon => (
+            <div className="pokemon" key={pokemon.id}>
+              <img
+                src={
+                  "https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/" +
+                  pokemon.id +
+                  ".png"
+                }
+                className="sprite"
+              />
+              <h3 className="pokemon-name">{pokemon.name}</h3>
+              <button
+                className="remove"
+                onClick={() => releasePokemon(pokemon.id)}
+              >
+                &times;
+              </button>
+            </div>
+          ))}
+        </div>
       </section>
     </div>
   );
