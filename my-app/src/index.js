92c88c5126a2518d7c59dd89b40bf318c1e22380
Jonatan <jonatanav255@hotmail.com>
2019-11-27 19:29:51 -0600

    axious

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import "./index.css";
 
 function App() {
+  const [pokedex, setPokedex] = useState([]);
+  const [wildPokemon, setWildPokemon] = useState({});
+
+  useEffect(() => {
+    encounterWildPokemon();
+  }, [pokedex]);
+  const encounterWildPokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/151").then(response => {
+      console.log(response.data);
+      setWildPokemon(response.data);
+    });
+  };
+
   return <div className="app-wrapper">Hello world</div>;
 }
 
